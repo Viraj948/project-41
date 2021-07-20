d8b6a4453143f8539dfb54106d7062aff7db5038
Viraj948 <74658897+Viraj948@users.noreply.github.com>
2021-07-20 15:51:05 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -105,7 +105,7 @@ class Game{
                     for (var i = 0; i < fruitGroup.length; i++) {
                         if (fruitGroup.get(i).isTouching(players)) {
                             fruitGroup.get(i).destroy();
-                            player.score =player.score+1;
+                          
                             
                         }
                         
